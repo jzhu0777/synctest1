bf2337cfd158db916524bb020787b68dc7a9206e
Junyu <zhujunyu168@hotmail.com>
2017-06-10 18:32:19 +1200

    fdsaf

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"strconv"
+
+	"github.com/juju/errors"
+)
+
+func main() {
+	n := 0
+	var cmd = ""
+	fmt.Println("start")
+	for n < 10000 {
+
+		cmd = "echo hello > ./" + strconv.Itoa(n) + ".txt"
+		_ = RunCMD(cmd)
+		fmt.Println(n)
+		n++
+	}
+	fmt.Println("done")
+}
+
+func RunCMD(cmd string) (err error) {
+	o := exec.Command("bash", "-c", cmd)
+	o.Stdout = os.Stdout
+	o.Stderr = os.Stderr
+	return errors.Trace(o.Run())
+}
